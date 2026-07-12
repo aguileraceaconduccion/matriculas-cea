5d15f653ca7408b8c6eb7932b0990aa48c26803f
aguileraceaconduccion <aguileraceaconduccion@gmail.com>
2026-07-11 20:44:49 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/matriculas-cea/" : "/",
   server: {
     host: "::",
     port: 8080,
